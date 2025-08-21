4894c063d38fec49deb8584c33de2dfd89befd74
selenbbilgic <sbilgic20@ku.edu.tr>
2025-08-21 23:53:55 +0300

    first commit

---
+++ PhotoGrouper/PhotoGrouper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoGrouper
+//
+//  Created by selen bilgiç on 21.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
